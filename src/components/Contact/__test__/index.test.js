50c391677394b1e5fe6777117d5b1628a5d8eebf
G.B.Abadio <gilabadio@gmail.com>
2021-01-01 23:36:57 -0500

    finished contact form test

---
+++ src/components/Contact/__test__/index.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('ContactForm component', () => {
+    //First Test
+    it('renders', () => {
+        render(<ContactForm />);
+    });
+
+    //Second Test
+    it('matches snapshot', () =>{
+        const {asFragment} = render(<ContactForm/>);
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
